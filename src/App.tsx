21bf9df36bf2ea22791383f3b865d6a9f813df14
juanfsouza <juanfsouza@example.com>
2025-11-21 17:59:48 -0300

    Git init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+export default function App() {
+
+  return (
+    <>
+      <div>
+        <p className="text-center text-xl">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  )
+}
